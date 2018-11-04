766dbccb7facc81816a7cb9633dcf58c86025a2a
Ali Reza <amosavi@ualberta.ca>
2018-11-04 16:12:24 -0700

    updated the offer ride function to include a scrolling function

--- miniproject1.py
+++ miniproject1.py
@@ -82,84 +82,6 @@ def printMessages(email):
 
 def offerRide(email):
 
-    is_valid_info = False
-    while not is_valid_info:
-        rdate = str(input("Please enter ride date: "))
-        seats = str(input("Please enter number of seats: "))
-        price = str(input("Please enter price per seat: "))
-        lugDesc = str(input("Please enter a luggage description: "))
-        src = str(input("Please enter a ride source: "))
-        dst = str(input("Please enter ride destination: "))
-        offer_info = [rdate, seats, price, lugDesc, src, dst, email]
-        valid_info_res = cursor.execute("INSERT INTO rides (rdate, seats, price, lugDesc, src, dst, driver) VALUES ((:offer_info))", {"offer_info":offer_info})
-        if valid_info_res:
-            is_valid_info = True
-
-    optional_cno_input = str(input("Would you like to add a car number? (y/n): "))
-    if optional_cno_input == 'y':
-        cno = str(input("Please enter your car number (cno): "))
-        cno_info = [cno, email, rdate]
-        cursor.execute("UPDATE rides SET cno = (:cno_info) WHERE rides.driver = (:cno_info) AND rides.rdate = (:cno_info)", {"cno_info":cno_info}) # update the cno for the logged in driver
-    elif optional_cno_input == 'n':
-        location_search_input = str(input("Please search for the appropriate lcode associate with your ride using lcode or using a keyword: "))
-        location_search_result = get_location("%" + location_search_input + "%")
-        if len(location_search_result) >= 5:
-            location_decision_input = str(input("The search returned more than 5 locations matching your query. Would you like to see them all? (y/n): "))
-            if location_decision_input == 'y':
-                print("Please select from the following list:\nlcode\tCity\tProvince\tAddress")
-                for i in range(len(location_search_result)):
-                    print(location_search_result[i])
-            elif location_decision_input == 'n':
-                print("Please select from the following list:\nlcode\tCity\tProvince\tAddress")
-                for j in range(5):
-                    print(location_search_result[j])
-            else:
-                print("Invalid input. Try again")
-
-        location_selection_input = str(input("lcode selection: "))
-        location_selection_res = cursor.execute("SELECT locations.lcode FROM locations WHERE locations.lcode = (:lcode)", {"lcode":location_selection_input})
-        
-        if location_selection_res:
-            rno = random.randint(1, 100000) # assign a random int to the rno
-            rno_info = [rno, rdate, email]
-            cursor.execute("UPDATE rides SET rno = (:rno_info) WHERE rdate = (:rno_info) AND driver = (:rno_info)", {"rno_info":rno_info}) # insert it into the rides table
-            print("rno has been updated") # print a success message
-        else:
-            print("That location does not exist or is invalid. Try again.")
-    else:
-        print("Invalid input. Try again.")
-
-    optional_enroute_input = str(input("Would you like to add an enroute location? (y/n): "))
-    if optional_enroute_input == 'y':
-        enroute_search_input = str(input("Please search for the appropriate lcode associate with your ride using lcode or a keyword: "))
-        enroute_search_result = get_location("%" + enroute_search_input + "%")
-        if len(enroute_search_result) >= 5:
-            enroute_decision_input = str(input("The search returned more than 5 locations matching your query. Would you like to see them all? (y/n): "))
-            if enroute_decision_input == 'y':
-                print("Please select from the following list:\nlcode\tCity\tProvince\tAddress")
-                for i in range(len(enroute_search_result)):
-                    print(enroute_search_result[i])
-            elif enroute_decision_input == 'n':
-                print("Please select from the following list:\nlcode\tCity\tProvince\tAddress")
-                for i in range(5):
-                    print(enroute_search_result[i])
-            else:
-                print("Invalid input. Try again.")
-
-        enroute_selection_input = str(input("lcode selection: "))
-        enroute_selection_res = cursor.execute("SELECT locations.lcode FROM locations WHERE locations.lcode = (:lcode)", {"lcode":enroute_selection_input})
-
-        if enroute_selection_res:
-            enroute_info = [rno, enroute_selection_input]
-            cursor.execute("INSERT INTO enroute (rno, lcode) VALUES (:enroute_info)", {"enroute_info":enroute_info}) #insert into enroute table where rno of location is the same
-            enroute_again_input = str(input("Enroute location added.\nWould you like to add another enroute location? (y/n): "))
-            if enroute_again_input == 'y':
-                optional_enroute_input == 'y'
-            elif enroute_again_input == 'n':
-                optional_enroute_input == 'n'
-            else:
-                print("Invalid input. Try again.")
-        
     # give rdate, seats, lugDesc, src, dst, price
     # optional: add a cno and enroute.lcode
     #   if cno is entered: query and check that the cno belongs to the member
@@ -176,76 +98,98 @@ def offerRide(email):
     # if a valid_selection == True (i.e. is confirmed): assign a random rno and set that offering member as the driver.
     # ride_offer =  # pass off the offering member's email
 
-# #try:
-#     cursor.execute("SELECT rides.rdate, rides.seats, rides.lugDesc, rides.src, rides.dst, rides.price FROM rides WHERE rides.driver = :email", {"email":email})
-#     #print("here")
-# #except:
-#     #pass
-#     #print("some error occurred")
-# #else:
-#     optional_input = str(input("would you like to add a car number and enroute locations? (y/n): "))
-#     valid_selection = False
-#     while not valid_selection:
-#         if optional_input == 'y':
-#             enroute_decision = False
-#             while not enroute_decision:
-#                 check_cno = "SELECT cars.cno, cars.owner, rides.cno, rides.driver FROM cars, rides WHERE cars.cno = rides.cno AND cars.owner = :email", {"email":email}
-#                 cursor.execute(check_cno)
-#                 if check_cno: # if the query returns True get the enroute location
-#                     enroute_input = str(input("enter a location code (lcode) or a location keyword: "))
-#                     enroute_location = get_location("%" + enroute_input + "%") # query the search with this function and return it
-#                     if len(enroute_location) >= 5:
-#                         see_more_enroute = str(input("There are more than 5 locations that match your query. Would you like to see them all? (y/n): "))
-#                         if see_more_enroute =='y':
-#                             print("lcode\tCity\tProvince\tAddress")
-#                             for i in range(len(enroute_location)):
-#                                 print(enroute_location[i])
-#                         elif see_more_enroute == 'n':
-#                             print("lcode\tCity\tProvince\tAddress\t")
-#                             for j in range(5):
-#                                 print(enroute_location[j])
-#                     valid_enroute_lcode = False
-#                     while not valid_enroute_lcode:
-#                         enroute_lcode_selection = str(input("Please select a lcode from above: "))
-#                         enroute_lcode_res = cursor.execute("SELECT * FROM locations WHERE locations.lcode = (:lcode)", {"lcode":enroute_lcode_selection})
-#                         if enroute_lcode_res:
-#                             cursor.execute("INSERT INTO enroute (lcode) VALUES ((:lcode))", {"lcode":enroute_lcode_selection})
-#                             valid_enroute_lcode = True
-#                         else:
-#                             print("That location does not exist. Please try again.")
-#                     enroute_decision_input = str(input("Would you like to add another enroute location? (y/n): "))
-#                     if enroute_decision_input == 'y':
-#                         enroute_decision = False
-#                     elif enroute_decision_input == 'n':
-#                         enroute_decision = True
-#             valid_selection = True
-#         elif optional_input == 'n':
-#             location_input = str(input("Enter a location code or a location keyword: "))
-#             location = get_location("%" + location_input + "%")
-#             #print(location)
-#             if len(location) >= 5:
-#                 see_more = str(input("There are more than 5 locations that match your query. Would you like to see them all? (y/n): "))
-#                 if see_more =='y':
-#                     print("lcode\tCity\tProvince\tAddress")
-#                     for i in range(len(location)):
-#                         print(location[i])
-#                 elif see_more == 'n':
-#                     print("lcode\tCity\tProvince\tAddress\t")
-#                     for j in range(5):
-#                         print(location[j])
-#             #print(location)
-#             #location_confirmation =
-#             #str(input("Please select from this list: %s", location))
-#             # book the enroute locations
-#             valid_selection = True
-#         else:
-#             print("Please enter a valid input")
-#     if valid_selection:
-#         rno = random.randint(0, 100000)
-#         #update_offer = 
-#         cursor.execute("INSERT INTO rides (email, rno) VALUES ((:email), (:rno))", {"email":email, "rno": rno})
-#         print("driver and ride number have been updated")
+    is_valid_info = False
+    while not is_valid_info: # until a successful query is executed, repeat
+        rdate = str(input("Please enter ride date: "))
+        seats = str(input("Please enter number of seats: "))
+        price = str(input("Please enter price per seat: "))
+        lugDesc = str(input("Please enter a luggage description: "))
+        src = str(input("Please enter a ride source: "))
+        dst = str(input("Please enter ride destination: "))
+        #offer_info = (rdate, seats, price, lugDesc, src, dst, email)
+        valid_info_res = cursor.execute("INSERT INTO rides (rdate, seats, price, lugDesc, src, dst, driver) VALUES ((:rdate), (:seats), (:price), (:lugDesc), (:src), (:dst), (:email))", {"rdate":rdate, "seats":seats, "price":price, "lugDesc":lugDesc, "src":src, "dst":dst, "email":email})
+        if valid_info_res:
+            connection.commit()
+            is_valid_info = True
 
+    is_valid_optional_cno = False
+    while not is_valid_optional_cno: # until a valid input is given, repeat
+        optional_cno_input = str(input("Would you like to add a car number? (y/n): "))
+
+        if optional_cno_input == 'y':
+            cno = str(input("Please enter your car number (cno): "))
+            #cno_info = [cno, email, rdate]
+            cursor.execute("SELECT * FROM cars WHERE cars.cno = (:cno) AND cars.owner = (:email)", {"cno":cno, "email":email})
+            if cursor.fetchone() is not None: # if the cno is associated to the owner, update it
+                cursor.execute("UPDATE rides SET cno = (:cno) WHERE rides.driver = (:email) AND rides.rdate = (:rdate)", {"cno":cno, "email":email, "rdate":rdate}) # update the cno for the logged in driver
+                connection.commit()
+            else:
+                optional_cno_try_again = str(input("That cno does not exist. Try again? (y/n): "))
+                if optional_cno_try_again == 'y':
+                    optional_cno_input = 'y'
+                elif optional_cno_try_again == 'n':
+                    print("cno not updated.")
+                    optional_cno_input = 'n' # move to the next step (get the location input)
+                
+        elif optional_cno_input == 'n':
+            is_valid_location_selection = False
+            while not is_valid_location_selection: # until a valid lcode is selected, repeat
+                location_search_input = str(input("Please search for the appropriate lcode associate with your ride using lcode or using a keyword: "))
+                location_search_result = split_list(get_location("%" + location_search_input + "%"), 5) # call the function to query the input
+
+                scroll(location_search_result) # allow the user to scroll through the results
+
+                location_selection_input = str(input("lcode selection: ")) # take an lcode selection
+                cursor.execute("SELECT locations.lcode FROM locations WHERE locations.lcode = (:lcode)", {"lcode":location_selection_input}) # query it
+                #connection.commit()
+                if cursor.fetchone() is not None: # test the query to see if it exists
+                    rno = random.randint(1, 100000) # assign a random int to the rno
+                    cursor.execute("UPDATE rides SET rno = (:rno) WHERE rdate = (:rdate) AND driver = (:email)", {"rno":rno, "rdate":rdate, "email":email}) # insert it into the rides table
+                    print("rno has been updated") # print a success message
+                    connection.commit()
+                    is_valid_location_selection = True 
+                else:
+                    print("That location does not exist or is invalid. Try again.")
+
+            is_valid_optional_cno = True
+
+        else:
+            print("Invalid input. Try again.")
+    is_valid_optional_enroute_input = False
+
+    while not is_valid_optional_enroute_input:
+        optional_enroute_input = str(input("Would you like to add an enroute location? (y/n): "))
+        if optional_enroute_input == 'y':
+            is_valid_enroute_location = False
+            while not is_valid_enroute_location:
+                enroute_search_input = str(input("Please search for the appropriate lcode associate with your ride using lcode or a keyword: "))
+                enroute_search_result = split_list(get_location("%" + enroute_search_input + "%"), 5) # split the list of the location list into n sized chunks
+
+                scroll(enroute_search_result) # allow user to scroll through the results
+
+                enroute_selection_input = str(input("lcode selection: "))
+                cursor.execute("SELECT locations.lcode FROM locations WHERE locations.lcode = (:lcode)", {"lcode":enroute_selection_input})
+
+                if cursor.fetchone() is not None:
+                    #enroute_info = [rno, enroute_selection_input]
+                    cursor.execute("INSERT INTO enroute (rno, lcode) VALUES ((:rno), (:enroute_selection_input))", {"rno":rno ,"enroute_selection_input":enroute_selection_input}) #insert into enroute table where rno of location is the same
+                    connection.commit()
+                    enroute_again_input = str(input("Enroute location added.\nWould you like to add another enroute location? (y/n): "))
+                    if enroute_again_input == 'y':
+                        optional_enroute_input = 'y'
+                    elif enroute_again_input == 'n':
+                        #optional_enroute_input = 'n'
+                        is_valid_enroute_location = True
+                        is_valid_optional_enroute_input = True
+                    else:
+                        print("Invalid input. Try again.")
+                else:
+                    print("That is an invalid lcode. Try again.")
+        elif optional_enroute_input == 'n':
+            print("No enroute location added.")
+            is_valid_optional_enroute_input = True
+        else:
+            print("Invalid input. Try again.")
 
 def get_location(location):
 
@@ -258,7 +202,39 @@ def get_location(location):
     else:
         return attempt
 
+def split_list(arr, n): # splits an arr into n sized chunks -> arr
 
+    return [arr[i * n:(i + 1) * n] for i in range((len(arr) + n - 1) // n)]
+
+def print_next(arr, last_iter, new_iter): # a function to print the next elements in an array -> None
+
+    for i in range(last_iter, new_iter):
+        for j in range(len(arr[i])):
+            print(arr[i][j])
+
+def scroll(arr): # a function to scroll through an arr of results -> None
+    see_more = False
+    from_iter = 0
+    up_to = 0
+    while (from_iter < len(arr) and up_to < len(arr) and not see_more):
+        up_to += 1
+        if len(arr[from_iter]) != 5: # if there are less than 5 results
+            for i in range(from_iter, up_to):
+                for j in range(len(arr[i])):
+                    print(arr[i][j])
+            print("End of results.")
+            see_more = True
+        else: # if there are more than 5
+            print_next(arr, from_iter, up_to) #results, prompt with an input message to see more
+            see_more_input = str(input("Press s to scroll down or press 0 to enter lcode: "))
+            if see_more_input == "s":
+                from_iter += 1
+                print_next(arr, from_iter, up_to)
+                #print(from_iter, up_to)                                    
+            elif see_more_input == "0":
+                see_more = True
+
+    
 def SearchRides():
     locVal = False
     while not locVal:
